af0b65486f85c20857a7a7f5e9959d90b706d717
BidyutBikashBoruah9365 <bidyutbikashboruah849@gmail.com>
2024-05-22 21:24:01 +0530

    quizApp

---
+++ script.js
@@ -0,0 +1,17 @@
+const questions = [
+    {
+        question:"What is the capital of Assam?",
+        options: ["Dispur", "Nagaland", "Aizawl", "Lakhimpur"],
+        correctAnswer: 0
+    },
+    {
+        question:"Bird is related to Wings in the same way as Fish is related to _______  ",
+        options: ["Belly", "Scales", "Fins", "Gills"],
+        correctAnswer: 2
+    },
+    {
+        question: "Select the word pair in which the two words are related in the same way as are the two words in the given word-pair. Small: Dimension ?",
+        options: ["Heat: Temperature", "Heavy: Weight", "Triangle: Area", "Cold: Winter"],
+        correctAnswer: 1
+    }
+]

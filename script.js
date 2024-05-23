d347e24bb3defb122042919105a04027fdafc136
BidyutBikashBoruah9365 <bidyutbikashboruah849@gmail.com>
2024-05-23 19:54:43 +0530

    quizApp

--- script.js
+++ script.js
@@ -1,17 +1,88 @@
-const questions = [
+const quizData = [
     {
-        question:"What is the capital of Assam?",
-        options: ["Dispur", "Nagaland", "Aizawl", "Lakhimpur"],
-        correctAnswer: 0
+        question: "What is the capital of France?",
+        a: "Berlin",
+        b: "Madrid",
+        c: "Paris",
+        d: "Lisbon",
+        correct: "c"
     },
     {
-        question:"Bird is related to Wings in the same way as Fish is related to _______  ",
-        options: ["Belly", "Scales", "Fins", "Gills"],
-        correctAnswer: 2
+        question: "Who is the CEO of Tesla?",
+        a: "Jeff Bezos",
+        b: "Elon Musk",
+        c: "Bill Gates",
+        d: "Tony Stark",
+        correct: "b"
     },
     {
-        question: "Select the word pair in which the two words are related in the same way as are the two words in the given word-pair. Small: Dimension ?",
-        options: ["Heat: Temperature", "Heavy: Weight", "Triangle: Area", "Cold: Winter"],
-        correctAnswer: 1
+        question: "What is the largest ocean on Earth?",
+        a: "Atlantic Ocean",
+        b: "Indian Ocean",
+        c: "Arctic Ocean",
+        d: "Pacific Ocean",
+        correct: "d"
+    },
+    {
+        question: "What year was JavaScript created?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "1993",
+        correct: "b"
     }
-]
+];
+
+const quiz = document.getElementById("quiz")
+const answerElements = document.querySelectorAll(".answer")
+const questionElement = document.getElementById("question")
+
+const atext = document.getElementById("aText")
+const btext = document.getElementById("bText")
+const ctext = document.getElementById("cText")
+const dtext = document.getElementById("dText")
+
+const submitButton = document.getElementById("submit")
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz(){
+    const currentQuizData = quizData[currentQuiz]
+    questionElement.innerText = currentQuizData.question
+
+    atext.innerText = currentQuizData.a
+    btext.innerText = currentQuizData.b
+    ctext.innerText = currentQuizData.c
+    dtext.innerText = currentQuizData.d
+}
+
+function getSelected(){
+    let answer
+    answerElements.forEach(ans => {
+        if( ans.checked){
+            answer = ans.id
+        }
+    })
+    return answer
+}
+
+submitButton.addEventListener("click", () => {
+    const answer = getSelected()
+    if(answer){
+        if(answer === quizData[currentQuiz].correct){
+            score++
+        }
+        currentQuiz++
+        if(currentQuiz < quizData.length){
+            loadQuiz()
+        }
+        else{
+            quiz.innerHTML = `<h2> You answered ${score}/${quizData.length} questions correctly </h2>
+            <button onclick = "location.reload()"> Reload </button>`
+        }
+    }
+})
+
